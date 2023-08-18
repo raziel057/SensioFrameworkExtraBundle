1980c7630630fed64794e00d5be5254695c4a1b2
Thomas Lallement <thomas.lallement@9online.fr>
2023-08-18 10:06:32 +0200

    Update AddParamConverterPass.php

--- src/DependencyInjection/Compiler/AddParamConverterPass.php
+++ src/DependencyInjection/Compiler/AddParamConverterPass.php
@@ -22,7 +22,7 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 class AddParamConverterPass implements CompilerPassInterface
 {
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
         if (false === $container->hasDefinition('sensio_framework_extra.converter.manager')) {
             return;
